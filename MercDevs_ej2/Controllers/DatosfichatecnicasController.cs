d69021aafa1218fae325484b94fec790cb2c7fa7
moontivac10n <lucas.venegas.jeldrezcftmail.cl>
2024-07-25 23:03:06 -0400

    Prueba finalizada

---
+++ MercDevs_ej2/Controllers/DatosfichatecnicasController.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MercDevs_ej2.Models;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MercDevs_ej2.Controllers
+{
+    public class DatosfichatecnicasController : Controller
+    {
+        private readonly MercyDeveloperContext _context;
+
+        public DatosfichatecnicasController(MercyDeveloperContext context)
+        {
+            _context = context;
+        }
+        [Authorize]
+        public async Task<IActionResult> FichaTecnica(int? id)
+        {
+            //Completar el código para la vista de FICHA TECNICA COMPLETA
+            //El ID recibido por este método es el id de Datos de Ficha Tecnica
+
+            var mercydevsEjercicio2Context = await _context.Datosfichatecnicas
+                .Where(d => d.IdDatosFichaTecnica == id)
+                .Include(d => d.RecepcionEquipo)
+                .Include(d => d.Diagnosticosolucions)
+                .Include(d => d.RecepcionEquipo.IdClienteNavigation)
+                .FirstOrDefaultAsync(d => d.RecepcionEquipoId == id);
+            return View(mercydevsEjercicio2Context);
+        }
+        [Authorize]
+        public async Task<IActionResult> Inicio()
+        {
+            var mercydevsEjercicio2Context = _context.Datosfichatecnicas.Include(d => d.RecepcionEquipo);
+            return View(await mercydevsEjercicio2Context.ToListAsync());
+        }
+
+        // GET: Datosfichatecnicas
+        [Authorize]
+        public async Task<IActionResult> Index(int id)
+        {
+            var fichaTecnica = await _context.Datosfichatecnicas
+                .Include(d => d.RecepcionEquipo)
+                .Include(d => d.Diagnosticosolucions)
+                .Include(d => d.RecepcionEquipo.IdClienteNavigation)
+                .FirstOrDefaultAsync(d => d.RecepcionEquipoId == id);
+
+            if (fichaTecnica == null)
+            {
+                return RedirectToAction("Create", new { id });
+            }
+
+            return View(fichaTecnica);
+        }
+
+        //Listar Datos ficha Tecnica por Recepción de Equipos de Cliente: VerDatosFichaTecnicaPorRecepcion
+        [Authorize]
+        public async Task<IActionResult> VerDatosFichaTecnicaPorRecepcion(int id)
+        {
+            var mercydevsEjercicio2Context = _context.Datosfichatecnicas
+                .Where(d => d.RecepcionEquipoId == id)
+                .Include(d => d.RecepcionEquipo);
+            ViewData["IdRecepcionEquipo"] = id;
+            return View(await mercydevsEjercicio2Context.ToListAsync());
+        }
+
+
+        // GET: Datosfichatecnicas/Diagnosticosolucionpordatosficha/5
+        [Authorize]
+        public async Task<IActionResult> Diagnosticosolucionpordatosficha(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var verdiagnostico = await _context.Datosfichatecnicas
+                .Include(r => r.Diagnosticosolucions)
+                .Include(r => r.RecepcionEquipo)
+                .Include(d => d.RecepcionEquipo.IdClienteNavigation)
+                .FirstOrDefaultAsync(m => m.IdDatosFichaTecnica == id);
+            if (verdiagnostico == null)
+            {
+                return NotFound();
+            }
+
+            return View(verdiagnostico);
+        }
+
+        // GET: Datosfichatecnicas/Details/5
+        [Authorize]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var datosfichatecnica = await _context.Datosfichatecnicas
+                .Include(d => d.RecepcionEquipo)
+                .FirstOrDefaultAsync(m => m.IdDatosFichaTecnica == id);
+            if (datosfichatecnica == null)
+            {
+                return NotFound();
+            }
+
+            return View(datosfichatecnica);
+        }
+
+        // GET: Datosfichatecnicas/Create
+        [Authorize]
+        public IActionResult Create(int? id)
+        {
+            ViewData["RecepcionEquipoId"] = new SelectList(_context.Recepcionequipos, "Id", "Id");
+            ViewData["IdRecepcionEquipo"] = id;
+            return View();
+        }
+
+        // POST: Datosfichatecnicas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int? id,[Bind("IdDatosFichaTecnica,FechaInicio,FechaFinalizacion,PobservacionesRecomendaciones,Soinstalado,SuiteOfficeInstalada,LectorPdfinstalado,NavegadorWebInstalado,AntivirusInstalado,RecepcionEquipoId")] Datosfichatecnica datosfichatecnica)
+        {
+            if (datosfichatecnica.FechaInicio != null)
+            {
+                datosfichatecnica.RecepcionEquipoId = Convert.ToInt32(id);
+                _context.Add(datosfichatecnica);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Recepcionequipoes");
+            }
+            ViewData["RecepcionEquipoId"] = new SelectList(_context.Recepcionequipos, "Id", "Id", datosfichatecnica.RecepcionEquipoId);
+            return View(datosfichatecnica);
+        }
+
+        // GET: Datosfichatecnicas/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var datosfichatecnica = await _context.Datosfichatecnicas.FindAsync(id);
+            if (datosfichatecnica == null)
+            {
+                return NotFound();
+            }
+            ViewData["RecepcionEquipoId"] = new SelectList(_context.Recepcionequipos, "Id", "Id", datosfichatecnica.RecepcionEquipoId);
+            return View(datosfichatecnica);
+        }
+
+        // POST: Datosfichatecnicas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdDatosFichaTecnica,FechaInicio,FechaFinalizacion,PobservacionesRecomendaciones,Soinstalado,SuiteOfficeInstalada,LectorPdfinstalado,NavegadorWebInstalado,AntivirusInstalado,RecepcionEquipoId")] Datosfichatecnica datosfichatecnica)
+        {
+            if (id != datosfichatecnica.IdDatosFichaTecnica)
+            {
+                return NotFound();
+            }
+
+            if (datosfichatecnica.FechaInicio != null)
+            {
+                try
+                {
+                    _context.Update(datosfichatecnica);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DatosfichatecnicaExists(datosfichatecnica.IdDatosFichaTecnica))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Inicio));
+            }
+            ViewData["RecepcionEquipoId"] = new SelectList(_context.Recepcionequipos, "Id", "Id", datosfichatecnica.RecepcionEquipoId);
+            return View(datosfichatecnica);
+        }
+
+        // GET: Datosfichatecnicas/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var datosfichatecnica = await _context.Datosfichatecnicas
+                .Include(d => d.RecepcionEquipo)
+                .FirstOrDefaultAsync(m => m.IdDatosFichaTecnica == id);
+            if (datosfichatecnica == null)
+            {
+                return NotFound();
+            }
+
+            return View(datosfichatecnica);
+        }
+
+        // POST: Datosfichatecnicas/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var datosfichatecnica = await _context.Datosfichatecnicas.FindAsync(id);
+            if (datosfichatecnica != null)
+            {
+                _context.Datosfichatecnicas.Remove(datosfichatecnica);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EnviarFichaTecnica(int id)
+        {
+            var fichaTecnica = await _context.Datosfichatecnicas
+                .Include(d => d.RecepcionEquipo)
+                .Include(d => d.RecepcionEquipo.IdClienteNavigation)
+                .FirstOrDefaultAsync(d => d.IdDatosFichaTecnica == id);
+
+            if (fichaTecnica == null)
+            {
+                return Json(new { success = false });
+            }
+
+            var cliente = fichaTecnica.RecepcionEquipo.IdClienteNavigation;
+            var enlace = Url.Action("FichaTecnicaPublica", "DatosFichaTecnicas", new { id = fichaTecnica.IdDatosFichaTecnica }, Request.Scheme);
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("felipe.castillo.barraza@cftmail.cl", "ihwwcraydabkowan"),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("felipe.castillo.barraza@cftmail.cl"),
+                Subject = "Su Ficha Técnica",
+                Body = $"Estimado/a {cliente.Nombre},\n\nPuede ver su ficha técnica en el siguiente enlace: {enlace}",
+                IsBodyHtml = false,
+            };
+            mailMessage.To.Add(cliente.Correo);
+
+            try
+            {
+                await smtpClient.SendMailAsync(mailMessage);
+                return Json(new { success = true });
+            }
+            catch
+            {
+                return Json(new { success = false });
+            }
+        }
+
+        public async Task<IActionResult> FichaTecnicaPublica(int id)
+        {
+            var fichaTecnica = await _context.Datosfichatecnicas
+                .Include(d => d.RecepcionEquipo)
+                .Include(d => d.RecepcionEquipo.IdClienteNavigation)
+                .Include(d => d.Diagnosticosolucions)
+                .FirstOrDefaultAsync(d => d.IdDatosFichaTecnica == id);
+
+            if (fichaTecnica == null)
+            {
+                return NotFound();
+            }
+
+            return View(fichaTecnica);
+        }
+
+        private bool DatosfichatecnicaExists(int id)
+        {
+            return _context.Datosfichatecnicas.Any(e => e.IdDatosFichaTecnica == id);
+        }
+    }
+}
